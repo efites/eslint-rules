bc23a607df6f43438222bf627e9c70f6b4e18a67
Mny <artem_molkov@senla.eu>
2024-12-09 16:42:49 +0300

    👑 create sort imports and empty lines after them

--- packages/react/src/test.js
+++ packages/react/src/test.js
@@ -1,9 +1,14 @@
+import Foo from './test2'
+
+import './App.css'
+
+import {one, two} from './test2'
+import {Boooooooo} from './test2'
+import {ifdsghiufdjsjdkf} from './test2'
+
+
 const FIVE = 5
 const X = 23
 const a = 78
-
-const Foo = () => {
-	return 'Foo'
-}
 
 [1, 2].filter(a => a === 2)
---
+++ packages/react/src/test2.js
@@ -0,0 +1,13 @@
+export const one = 5
+export const two = 23
+export const ifdsghiufdjsjdkf = 78
+
+const Foo = () => {
+	return 'Foo'
+}
+
+export const Boooooooo = () => {
+	return 'Foo'
+}
+
+export default Foo
