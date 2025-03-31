bc021b808f567ad044dc55b8e1b9b9f00bb0778d
Mny <artem_molkov@senla.eu>
2025-03-31 15:58:35 +0300

    👑 prepare scripts for working

--- packages/myeslint/packages/eslint-config-inspector-bun/src/index.ts
+++ packages/myeslint/packages/eslint-config-inspector-bun/src/index.ts
@@ -1,44 +1,15 @@
 #!/usr/bin/env node
-import 'dotenv/config'
-import cors from 'cors'
-import express from 'express'
-import path, {dirname} from 'path'
-import {fileURLToPath} from 'url'
-import {websocket} from './src/websocket.js'
-import {router} from './src/routes/routes.js'
-import {getCorsOptions} from './src/helpers/cors.js'
+import {createApp, createRouter, defineEventHandler} from "h3"
+
+export const app = createApp()
 
 
-export const SERVER_URL = process.env.SERVER_URL || 'http://localhost'
-export const SERVER_PORT = process.env.SERVER_PORT || 8765 || 3344
-export const INSPECTOR_PORT = process.env.INSPECTOR_PORT || 7654 || 4455
-export const WEBSOCKET_PORT = process.env.APP_PORT || 6543 || 5566
+const router = createRouter()
+app.use(router)
 
-// Init an app
-const app = express()
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-// Routes
-app.use(express.static(path.join(__dirname, 'app', 'dist')))
-app.use('/', router)
-app.use(cors(getCorsOptions()))
-
-// Start
-const serverApp = app.listen(SERVER_PORT, () => {
-	websocket()
-
-	console.log(`✔️\tOpen the inspector: \t${SERVER_URL}:${SERVER_PORT}/`)
-})
-
-serverApp.on('error', (error) => {
-	if (error.code === 'EADDRINUSE') {
-		console.log(`Порт уже занят! Не буду поднимать сервер.`)
-
-		return
-	} else {
-		console.error('Какая-то другая ошибка при запуске сервера:', error)
-
-		return
-	}
-})
+router.get(
+	"/",
+	defineEventHandler((event) => {
+		return {message: "⚡️ Tadaa!!"}
+	}),
+)
