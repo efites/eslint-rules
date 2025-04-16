a3e1b7a673ab7348f70b113c65dd5ef028e83899
Mny <artem_molkov@senla.eu>
2025-04-16 16:06:32 +0300

    👑 create swc builder for commonjs

--- packages/myeslint/packages/eslint-config-inspector-bun/src/index.ts
+++ packages/myeslint/packages/eslint-config-inspector-bun/src/index.ts
@@ -4,21 +4,23 @@ import {config} from 'dotenv'
 import {createServer} from 'node:http'
 import {readFile, stat} from 'node:fs/promises'
 import {lookup} from 'mrmime'
-import {createApp, defineEventHandler, serveStatic, toNodeListener, createRouter} from "h3"
+import {createApp, defineEventHandler, serveStatic, toNodeListener} from "h3"
 import {router} from './routes/routes'
+import {websocket} from './helpers/websocket'
 
 
 // Env. variables
-config({path: path.resolve(__dirname, '../.env')})
+config({path: path.resolve(__dirname, '..', '..', '..', '.env')})
 
 export const app = createApp()
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL
-export const SERVER_PORT = import.meta.env.VITE_SERVER_PORT
+export const SERVER_URL = process.env.VITE_SERVER_URL
+export const SERVER_PORT = process.env.VITE_SERVER_PORT
+export const WEBSOCKET_PORT = process.env.VITE_WEBSOCKET_PORT
 
 app.use(router)
 app.use(
 	defineEventHandler((event) => {
-		const staticPath = path.join(__dirname, '..', 'dist', 'app')
+		const staticPath = path.join(__dirname, '..', '..', '..', 'dist', 'app')
 		console.log(staticPath)
 
 		return serveStatic(event, {
@@ -45,6 +47,7 @@ const server = createServer(toNodeListener(app))
 
 server.listen(SERVER_PORT, async () => {
 	const url = `http://localhost:${SERVER_PORT}`
+	websocket().catch(ex => console.log('Websocker Error: ', ex, '\n'))
 
 	console.log(`Starting eslint-config-bun at`, url, '\n')
 })
--- packages/myeslint/packages/eslint-config-inspector-bun/src/routes/routes.ts
+++ packages/myeslint/packages/eslint-config-inspector-bun/src/routes/routes.ts
@@ -1,69 +1,21 @@
-import path from 'node:path'
-import {createRouter, defineEventHandler, handleCors, serveStatic} from "h3"
-import {readFile, stat} from 'node:fs/promises'
-import {lookup} from 'mrmime'
+import {createRouter, defineEventHandler} from "h3"
 import {buildFileTree} from '../helpers/buildFileTree'
 import {getFilePathById} from '../helpers/getFilePathById'
 import {analyzeFile} from '../helpers/analyzeFile'
 import {getConfig} from '../helpers/getConfig'
-import {SERVER_PORT, SERVER_URL} from '../index'
 
-
-const whitelist = [`${SERVER_URL}:${SERVER_PORT}/`, `${SERVER_URL}:5173/`, 'http://localhost:3000']
 
 const router = createRouter()
-
-router.use('/**',
-	defineEventHandler(async (event) => {
-		const didHandleCors = handleCors(event, {
-			origin: function (origin) {
-				console.log('origin', origin)
-				if (whitelist.indexOf(origin) !== -1 || !origin) {
-					return true
-				}
-				return false
-			},
-			preflight: {
-				statusCode: 204,
-			},
-			methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],  // Разрешенные методы
-			allowHeaders: ['X-Requested-With', 'content-type'], // Разрешённые заголовки
-			credentials: true, // Разрешаем передачу кук и заголовков авторизации
-		})
-		console.log('didHandleCors', didHandleCors)
-		if (didHandleCors) {
-			return
-		}
-		// Your code here
-
-
-		/* const getCorsOptions = () => {
-			const whitelist = [`${SERVER_URL}:${SERVER_PORT}/`, `${SERVER_URL}:5173/`]
-
-			const corsOptions = {
-				origin: function (origin, callback) {
-					if (whitelist.indexOf(origin) !== -1 || !origin) {
-						callback(null, true)
-					} else {
-						console.log(origin)
-						callback(new Error('Not allowed by CORS'))
-					}
-				}, // Запросы только с этого источника
-				methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',  // Разрешенные методы
-				allowedHeaders: 'X-Requested-With,content-type', // Разрешённые заголовки
-				credentials: true, // Разрешаем передачу кук и заголовков авторизации
-				optionsSuccessStatus: 204 //  Некоторые старые браузеры (IE11) требуют 204 для OPTIONS
-			}
-
-			return corsOptions
-		} */
-	})
-)
 
 router.get(
 	'/tree',
 	defineEventHandler(async (event) => {
-		console.log('get tree')
+		// TODO - хардкод url
+		event.node.res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+		event.node.res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+		event.node.res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
+		event.node.res.setHeader('Access-Control-Allow-Credentials', 'true')
+
 		const projectRoot = process.cwd()
 		const config = await getConfig()
 		const fileTree = await buildFileTree(projectRoot, ['node_modules'], config)
@@ -73,6 +25,12 @@ router.get(
 ).get(
 	'/tree/:id',
 	defineEventHandler(async (event): Promise<ReturnType<typeof analyzeFile> | null> => {
+		// TODO - хардкод url
+		event.node.res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+		event.node.res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+		event.node.res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type')
+		event.node.res.setHeader('Access-Control-Allow-Credentials', 'true')
+
 		const id = event.context.params?.id
 
 		if (!id) return null
