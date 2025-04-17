80c360d190a750f11ec2398167c122d5db8eabc7
Mny <artem_molkov@senla.eu>
2025-04-17 09:07:11 +0300

    👑 try to add my eslint-config

--- packages/react/eslint.config.js
+++ packages/react/eslint.config.js
@@ -1,6 +1,12 @@
-export default [{
+import {eslint} from '@efites/eslint-config'
+
+
+
+export default eslint()
+
+/* export default [{
 	files: ["src/**"],
 	rules: {
 		'no-unused-vars': "error"
 	}
-}]
+}] */
